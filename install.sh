c25013331cb827e200f356ed1d90ca31327a37a3
Áldó <73642336+miatoszs@users.noreply.github.com>
2024-03-23 17:08:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/
 echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared jammy main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
 
 # install cloudflared
-sudo apt-get update && sudo apt-get install cloudflared
+sudo apt-get update && sudo apt-get install cloudflared php
